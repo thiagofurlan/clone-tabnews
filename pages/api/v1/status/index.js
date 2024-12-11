556a4735a27b0e12e9cb67857d2e38973745a61a
Thiago Furlan <thiago.sfurlan@outlook.com>
2024-12-11 20:27:44 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,24 @@
 import database from "infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ message: result });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = "local_db"; //process.env.POSTGRES_DB;
+  const serverVersion = await database.query("SHOW server_version;");
+  const serverMaxConnections = await database.query("SHOW max_connections;");
+  const openedMaxConnections = await database.query({
+    text: `SELECT COUNT(*)::int as opened_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        server_version: serverVersion.rows[0].server_version,
+        max_connections: parseInt(serverMaxConnections.rows[0].max_connections),
+        opened_connections: openedMaxConnections.rows[0].opened_connections,
+      },
+    },
+  });
 }

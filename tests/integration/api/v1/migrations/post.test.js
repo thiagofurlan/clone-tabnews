584bee43d96ed2b20773fc6d7cb413017c64372a
Thiago Furlan <thiago.sfurlan@outlook.com>
2025-03-24 14:33:12 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,21 +7,32 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const makePost = async () =>
-    await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const makePost = async () =>
+          await fetch("http://localhost:3000/api/v1/migrations", {
+            method: "POST",
+          });
+
+        const firstResponse = await makePost();
+        expect(firstResponse.status).toBe(201);
+        const firstResponseBody = await firstResponse.json();
+        expect(Array.isArray(firstResponseBody)).toBe(true);
+        expect(firstResponseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const makePost = async () =>
+          await fetch("http://localhost:3000/api/v1/migrations", {
+            method: "POST",
+          });
+        const secondResponse = await makePost();
+        expect(secondResponse.status).toBe(200);
+        const secondResponseBody = await secondResponse.json();
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
     });
-
-  const firstResponse = await makePost();
-  expect(firstResponse.status).toBe(201);
-  const firstResponseBody = await firstResponse.json();
-  expect(Array.isArray(firstResponseBody)).toBe(true);
-  expect(firstResponseBody.length).toBeGreaterThan(0);
-
-  const secondResponse = await makePost();
-  expect(secondResponse.status).toBe(200);
-  const secondResponseBody = await secondResponse.json();
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
+  });
 });

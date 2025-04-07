d988f3ce7488eb57a6dfdc5b86405693d1b88f6b
Thiago Furlan <thiago.sfurlan@outlook.com>
2025-04-07 19:23:35 +0000

    feat: add `methodnotallowederror` and `serviceError``

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,11 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("An unexpected error occurred", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Contact support";
-    this.status_code = 500;
+    this.status_code = statusCode || 500;
   }
 
   toJSON() {
@@ -14,6 +14,44 @@ export class InternalServerError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.status_code,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service unavailable", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Verify if the service is running";
+    this.status_code = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.status_code,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method not allowed for this endpoint");
+    this.name = "MethodNotAllowedError";
+    this.action = "Verify if the method is correct for this endpoint";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
     };
   }
 }

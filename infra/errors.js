ce6198be10df815452fcffa47fd16448f3b26aae
Thiago Furlan <thiago.sfurlan@outlook.com>
2025-07-07 13:54:36 +0000

    feat: create and use NotFoundError in controller.js

--- infra/errors.js
+++ infra/errors.js
@@ -75,3 +75,23 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Não foi possível encontrar o recurso solicitado.", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action = action || "Verifique o recurso e tente novamente.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
